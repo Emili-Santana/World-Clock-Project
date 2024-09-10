a0d15315b889b068b461623622ba2e19ffd0dcb4
Emili-Santana <santanaemili@gmail.com>
2024-09-10 18:00:03 +0100

    updaded

--- javascript/index.js
+++ javascript/index.js
@@ -51,7 +51,7 @@ function updatecity(event) {
             <div>
                 <h2>${cityName} </h2>
                 <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-            </div>
+                </div>
             <div class="time"> ${cityTime.format(
               "h:mm:ss"
             )} <small> ${cityTime.format("A")}</small></div>
